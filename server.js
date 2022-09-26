4387cda4ec078053d799424ef18b020a86221647
liutianhao <liutianhao328@gmail.com>
2022-09-26 16:43:01 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use('', express.static('./'))
+
+  // 监听端口
+http.listen(3001, function(){
+});
